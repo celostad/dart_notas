baa72f0aa75b41801917adba024ae3e06b0aac0b
Marcelo Tadim <marcelo.tadim@lowcost.com.br>
2025-04-02 20:17:18 -0300

    feature/aula5 - Adicionando uma nota na aplicação - video3

--- bin/notas.dart
+++ bin/notas.dart
@@ -1,21 +1,37 @@
 import 'dart:io';
 
 void main() {
+  List<String> notas = <String>[];
   getComando();
+  adicionaNota(notas);
 }
 
-String getComando(){
+String getComando() {
   print("Digite um comando: 1 - Adicionar nota, 2 -Listar notas, 3 - Sair");
   List<String> comandos = <String>["1", "2", "3"];
   String? entrada = "";
 
   entrada = stdin.readLineSync();
 
-  if(entrada == null || !comandos.contains(entrada)){
+  if (entrada == null || !comandos.contains(entrada)) {
     print("Comando inválido");
     getComando();
   }
 
-
   return entrada!;
+}
+
+List<String> adicionaNota(List<String> notas) {
+  print("Escreva uma nota:");
+  String? nota = "";
+
+  nota = stdin.readLineSync();
+
+  if (nota == null || nota.isEmpty) {
+    print("Não é possível adiconar uma nota vazia");
+    adicionaNota(notas);
+  }
+
+  notas.add(nota!);
+  return notas;
 }
